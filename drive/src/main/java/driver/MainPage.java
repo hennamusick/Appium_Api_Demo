1e2d78c1c3ccbde143a23c79300992e1f8b06aa1
Henna <Henna@DESKTOP-OLQP3VH>
2020-04-05 10:56:19 +0530

    Handling Locators - xpath, id, className, texts and indexes

---
+++ drive/src/main/java/driver/MainPage.java
@@ -0,0 +1,32 @@
+package driver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class MainPage {
+
+	public static AndroidDriver<AndroidElement> capabilities() throws MalformedURLException {
+
+		/*
+		 * File f = new File("src"); File fs = new File(f, "original.apk");
+		 */
+
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "6.0");
+		cap.setCapability(MobileCapabilityType.APP, "C:\\apkfiles\\ApiDemos-debug.apk");
+
+		URL url = new URL("http://127.0.0.1:4723/wd/hub");
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(url, cap);
+
+		return driver;
+	}
+}
